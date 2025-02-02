694b8fc2a0483fbfb7b674852724aab9a78cdf3e
Mobiledev <martinbennett0324@gmail.com>
2025-02-02 10:10:42 -0400

    Magnusbilling convert to Yii2

---
+++ views/partials/head-css.php
@@ -0,0 +1,16 @@
+<!-- Layout config Js -->
+<script src="<?= Yii::$app->request->baseUrl ?>//js/layout.js"></script>
+<!-- Bootstrap Css -->
+<link href="<?= Yii::$app->request->baseUrl ?>//css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<!-- Icons Css -->
+<link href="<?= Yii::$app->request->baseUrl ?>//css/icons.min.css" rel="stylesheet" type="text/css" />
+<!-- App Css-->
+<link href="<?= Yii::$app->request->baseUrl ?>//css/app.min.css" rel="stylesheet" type="text/css" />
+<!-- custom Css-->
+<link href="<?= Yii::$app->request->baseUrl ?>//css/custom.min.css" rel="stylesheet" type="text/css" />
+<link href="<?= Yii::$app->request->baseUrl ?>//css/style.css" rel="stylesheet" type="text/css" />
+
+<link href="<?= Yii::$app->request->baseUrl ?>//css/toastr/toastr.css" rel="stylesheet">
+<script src="<?= Yii::$app->request->baseUrl ?>//css/toastr/toastr.min.js"></script>
+<meta name="csrf-param" content="<?= Yii::$app->request->csrfParam ?>">
+<meta name="csrf-token" content="<?= Yii::$app->request->csrfToken ?>">
